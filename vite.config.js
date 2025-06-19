9feec3fbdf97aca3aca6ca47d9f1c6df6d251dab
sufiyan903 <131009052+sufiyan903@users.noreply.github.com>
2025-06-19 13:08:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    dedupe: ['framer-motion'], // Helps Vite resolve it correctly
-  },
-  build: {
-    rollupOptions: {
-      external: ['framer-motion'], // Prevent Rollup from mis-handling this import
-    },
+    dedupe: ['framer-motion'], // Ensures single instance
   },
 });
